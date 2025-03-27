d0b02b15619764572ddecc60130b8cec6823b898
malex1306 <alexandremarcel350@gmail.com>
2025-03-27 15:17:21 +0100

    new projects

---
+++ Kappselung/Bankkonto.cs
@@ -0,0 +1,48 @@
+namespace Kappselung;
+
+public class Bankkonto
+{
+    public int kontonummer { get; private set; }
+
+    private decimal kontostand;
+    
+    public int Kontonummer{get => kontonummer; set{kontonummer=value;} }
+
+    public Bankkonto(int kontonummer, decimal kontostand)
+    {
+        this.kontonummer = kontonummer;
+        this.kontostand = kontostand;
+    }
+
+    public decimal GetKontostand()
+    {
+        return kontostand;
+    }
+
+    public void Einzahlen(decimal betrag)
+    {
+        if (betrag > 0)
+        {
+            kontostand += betrag;
+            Console.WriteLine($"Einzahlung von Betrag {betrag} war erfolgreich. Neuer Kontostand: {kontostand}");
+        }
+        else
+        {
+            Console.WriteLine("Betrag muss positiv sein");
+        }
+    }
+
+    public void Abheben(decimal betrag)
+    {
+        if (betrag > 0 && betrag <= kontonummer)
+        {
+            kontostand -= betrag;
+            Console.WriteLine($"Abhebung von {betrag} erfolgreich. Neuer Kontostand: {kontostand}");
+        }
+        else
+        {
+            Console.WriteLine("Ungültiger Betrag oder unzureichender Kontostand.");
+        }
+    }
+    
+}
---
+++ Kappselung/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Kappselung;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Bankkonto meinKonto = new Bankkonto(12345678, 1000m);
+        Console.WriteLine($"Kontostand: {meinKonto.GetKontostand()}");
+        meinKonto.Abheben(500);
+        meinKonto.Einzahlen(1000);
+        Console.WriteLine($"Endgültiger Kontostand: {meinKonto.GetKontostand()}");
+        
+    }
+}
